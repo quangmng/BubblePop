b61b87729ad27960f66bc19f31190819c41537f6
Quang Minh Nguyen <quangng711@outlook.com.au>
2024-04-15 14:21:33 +1000

    Fixing up main menu... with some progress :)

---
+++ BubblePop/Model/Bubbles.swift
@@ -0,0 +1,8 @@
+//
+//  Bubbles.swift
+//  BubblePop
+//
+//  Created by Quang Minh Nguyen on 15/4/2024.
+//
+
+import Foundation
---
+++ BubblePop/View/PopupNameView.swift
@@ -0,0 +1,18 @@
+//
+//  PopupNameView.swift
+//  BubblePop
+//
+//  Created by Quang Minh Nguyen on 15/4/2024.
+//
+
+import SwiftUI
+
+struct PopupNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopupNameView()
+}
